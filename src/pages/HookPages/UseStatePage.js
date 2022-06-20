eed17f628e0151488fe4dabe5d5d2d94ee8df252
knguyen111601 <knguyen111601@gmail.com>
2022-06-20 13:15:05 -0700

    intro to useState hook

--- src/pages/HookPages/UseStatePage.js
+++ src/pages/HookPages/UseStatePage.js
@@ -5,7 +5,17 @@ const UseStatePage = () => {
     let location = useLocation()
     return <>
         {location.pathname !== "/" ? <Link to="/">Go Home</Link> : null}
-        <h1>useState</h1>
+        <h1 class="display-1">useState</h1>
+        <p class="lead">
+            The React <span style={{color:"red"}}>useState</span> Hook allows us to track state in a function component.
+        </p>
+        <dt class="col-sm-3">State</dt>
+        <dd class="col-sm-9">
+            <p>Refers to data or properties that need to be tracked in an application</p>
+        </dd>
+
+        
+
     </>
 }
 
